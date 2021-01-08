bf8f0e0b97cfa820e8e4e7e0e894fbb93d93add8
Mina97 <tkarnrwl78627862@gmail.com>
2021-01-08 15:50:51 +0900

    react_native_element_UI_sample updated

--- testapp/App.js
+++ testapp/App.js
@@ -5,6 +5,9 @@
  * @format
  * @flow
  */
+import { Dimensions } from 'react-native';
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 import React, {Fragment} from 'react';
 import {
@@ -35,31 +38,24 @@ const App = () => {
           <Header />
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+              <Text style={styles.sectionTitle}>튼sds</Text>
+       
             </View>
+
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
+              <Text style={styles.sectionTitle}>튼sds</Text>
+       
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -69,7 +65,8 @@ const App = () => {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: Colors.lighter,
+    //backgroundColor: Colors.lighter,
+    height:windowHeight
   },
   body: {
     backgroundColor: Colors.white,
