39ca22af433077ec529b20d44489fc9aba2b6d92
Mina97 <tkarnrwl78627862@gmail.com>
2021-01-08 11:53:50 +0900

    react_native_element_UI_sample updated

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-cd starter && sh upload.sh && cd .. && git add . && git commit -m "react_native_element_UI_sample updated" && git push
+git add . && git commit -m "react_native_element_UI_sample updated" && git push
